c45195975fba12d42f6b9f8f296f36f0c4a46339
Alex Sánchez C <asmalexander@hotmail.com>
2022-10-22 21:04:57 -0600

    Funcionalidad para realizar un movimiento en cualquier parte del laberinto
    Funcionalidad para realizar un movimiento por cualquier parte del laberinto.

--- app/main.pl
+++ app/main.pl
@@ -1,45 +1,52 @@
-row(N, Matrix, Row) :- nth0(N, Matrix, Row).
-
-get :- row(N, [[1,2],[3,3]], [A|B]), write(A).
-
-col(N,Col) :- X = [[1,2,3],[23,4,5],[1,3,5]],
-    maplist(nth1(N), X, Col).
-
-
-
-movimiento(X, Y, Direccion).
-
 % Arriba
-aMovimiento(mov(X, Y), ar, p(nuevoX, Y)) :-
-	nuevoX is X - 1.
+aMovimiento(ar) :-
+    posicion(I, J),
+    X is I,
+    Y is J,
+	NuevoX is X - 1,
+    retractall(posicion(_,_)),
+    asserta(posicion(NuevoX, Y)).
 
 % Abajo
-aMovimiento(mov(X,Y), ab, p(nuevoX, Y)) :-
-	nuevoX is X + 1.
+aMovimiento(ab) :-
+    posicion(I, J),
+    X is I,
+    Y is J,
+	NuevoX is X + 1,
+    retractall(posicion(_,_)),
+    asserta(posicion(NuevoX, Y)).
 
 % Izquierda
-aMovimiento(mov(X,Y), at, p(X, nuevoY)) :-
-	nuevoY is Y - 1.
+aMovimiento(at) :-
+    posicion(I, J),
+    X is I,
+    Y is J,
+	NuevoY is Y - 1,
+    retractall(posicion(_,_)),
+    asserta(posicion(X, NuevoY)).
 
 % Derecha
-aMovimiento(  mov(X,Y), ad, p(X, nuevoY)) :-
-	nuevoY is Y + 1.
+aMovimiento(ad) :-
+    posicion(I, J),
+    X is I,
+    Y is J,
+	NuevoY is Y + 1,
+    retractall(posicion(_,_)),
+    asserta(posicion(X, NuevoY)).
 
 verificarMovimiento(mov(X,Y)) :-
     X >=0, 
     Y >=0.
 
-
+main :- retractall(fila(_)), retractall(posicion(_,_)), leerArchivo, buscarInicio.
 
 leerArchivo:-
-    retractall(fila(_)),
     write("Ingrese la ubicacion del archivo: "),
     read(M),
     open(M,read,Str),
     obtenerLaberinto(Str,Matriz),
     close(Str),
-    asserta(matriz(Matriz)),
-    buscarInicio.
+    asserta(matriz(Matriz)).
    
 obtenerLaberinto(Stream,[]) :-
    at_end_of_stream(Stream).
@@ -47,6 +54,8 @@ obtenerLaberinto(Stream,[]) :-
 obtenerLaberinto(Stream,[X|L]) :-
     read(Stream,X),
     obtenerLaberinto(Stream,L).
+
+movimiento :- write("donde: "), read(M), aMovimiento(M).
 
 
 buscarInicio :- matriz([A|B]), member(i, A), I is 0, J is 0, asserta(posicion(I, J)).
@@ -62,3 +71,5 @@ buscarInicio3([A|B], I, J) :- A =\= i, R is J+1, buscarInicio3(B, I, R).
 im :- matriz([A|B]), member(i, A).
 im2([A|B]) :- write(A),nl, im2(B).
 im2([]).
+
+

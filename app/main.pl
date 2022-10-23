580f224efd098a426bd082683c04dfd836a2aefd
Alex Sánchez C <asmalexander@hotmail.com>
2022-10-22 20:47:50 -0600

    Funcionalidad para ubicarse en el inicio del laberinto
    Se agrego la funcionalidad para encontrar el inicio del laberinto.

--- app/main.pl
+++ app/main.pl
@@ -5,33 +5,60 @@ get :- row(N, [[1,2],[3,3]], [A|B]), write(A).
 col(N,Col) :- X = [[1,2,3],[23,4,5],[1,3,5]],
     maplist(nth1(N), X, Col).
 
-abrirArchivo :- read(M), open(M, read,Hola), read(Hola, H1), close(Hola), write(H1).
+
+
+movimiento(X, Y, Direccion).
+
+% Arriba
+aMovimiento(mov(X, Y), ar, p(nuevoX, Y)) :-
+	nuevoX is X - 1.
+
+% Abajo
+aMovimiento(mov(X,Y), ab, p(nuevoX, Y)) :-
+	nuevoX is X + 1.
+
+% Izquierda
+aMovimiento(mov(X,Y), at, p(X, nuevoY)) :-
+	nuevoY is Y - 1.
+
+% Derecha
+aMovimiento(  mov(X,Y), ad, p(X, nuevoY)) :-
+	nuevoY is Y + 1.
+
+verificarMovimiento(mov(X,Y)) :-
+    X >=0, 
+    Y >=0.
+
+
 
 leerArchivo:-
+    retractall(fila(_)),
     write("Ingrese la ubicacion del archivo: "),
     read(M),
     open(M,read,Str),
     obtenerLaberinto(Str,Matriz),
     close(Str),
-    buscarInicio(Matriz, 0).
+    asserta(matriz(Matriz)),
+    buscarInicio.
    
 obtenerLaberinto(Stream,[]) :-
    at_end_of_stream(Stream).
    
 obtenerLaberinto(Stream,[X|L]) :-
-    \+  at_end_of_stream(Stream),
     read(Stream,X),
     obtenerLaberinto(Stream,L).
 
-buscarInicio([A|B], Cont) :- verificarInicio(A, Cont), write("se encontro "), write(A), write(Cont).
-buscarInicio([A|B], Cont) :- not(verificarInicio(A, Cont)),R1 is Cont+1, buscarInicio(B, R1).
-buscarInicio([]) :- write("No se encontro el inicio del laberinto").
 
-verificarInicio([A|B], Cont) :- A == i.
-verificarInicio([A|B], Cont) :- verificarInicio(B, R1).
-verificarInicio([], Cont) :- false.
+buscarInicio :- matriz([A|B]), member(i, A), I is 0, J is 0, asserta(posicion(I, J)).
+buscarInicio :- matriz([A|B]), not(member(i, A)), I is 1, J is 0, buscarInicio2(B, I, J).
+
+buscarInicio2([A|B], I, J) :- member(i, A), buscarInicio3(A, I, J).
+buscarInicio2([A|B], I, J) :- not(member(i, A)), R is I+1, buscarInicio2(B, R, J).
+
+buscarInicio3([A|B], I, J) :- A == i, asserta(posicion(I, J)).
+buscarInicio3([A|B], I, J) :- A =\= i, R is J+1, buscarInicio3(B, I, R).
 
 
-imprimir1([A|B]) :- imprimir2(A).
-imprimir2([A|B]) :- A == x, write("es igual").
-imprimir2([A|B]) :- A =\= "x", write("no es igual").
+im :- matriz([A|B]), member(i, A).
+im2([A|B]) :- write(A),nl, im2(B).
+im2([]).

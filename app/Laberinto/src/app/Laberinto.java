c7c160737745305d623bf957fead3e011c2a3aa3
Alex Sánchez C <asmalexander@hotmail.com>
2022-10-31 03:20:24 -0600

    Cambios en la clase principal
    Se realizaron cambios en la documentacion de la clase principal.

--- app/Laberinto/src/app/Laberinto.java
+++ app/Laberinto/src/app/Laberinto.java
@@ -1,16 +1,16 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Nombre del paquete: app
  */
 package app;
 
-import javax.swing.JDialog;
 import vista.VentanaInicio;
+
+//Inclusion de librerias y clases
 import modelo.Modelo;
 
 /**
  *
- * @author asmal
+ * @author Alex Sánchez Céspedes
  */
 public class Laberinto {
 
@@ -24,24 +24,6 @@ public class Laberinto {
         modelo.ConectarPL();
         VentanaInicio ventanaInicio = new VentanaInicio();
         ventanaInicio.setVisible(true);
-
-        /*
-        
-        Query q1 = new Query( "consult", new Term[] {new Atom("C:\\Users\\asmal\\eclipse-workspace\\Prueba2\\src\\prueba.pl")} );
-	System.out.println( "consult " + (q1.hasSolution() ? "succeeded" : "failed"));
-        Query q2 = new Query( "child_of", new Term[] {new Atom("joe"),new Atom("ralf")} );
-	System.out.println( "child_of(joe,ralf) is " + ( q2.hasSolution() ? "provable" : "not provable" ) );
-	System.out.println(q2.hasSolution());
-		
-	Variable X = new Variable("X");
-	Query q4 = new Query( "descendent_of", new Term[] {X,new Atom("ralf")} );
-
-	java.util.Map<String,Term> solution;
-
-	solution = q4.oneSolution();
-
-	System.out.println( "first solution of descendent_of(X, ralf)"); 
-	System.out.println( "X = " + solution.get("X"));*/
     }
     
 }
